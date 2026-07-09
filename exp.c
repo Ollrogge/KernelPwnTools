6b92bd3ea4c1d5c7fa7d1a0f8ea6c7bad45dc26b
Ollrogge <nils-ollrogge@outlook.de>
2026-07-09 21:37:09 +0200

    remove constants I never use

--- exp.c
+++ exp.c
@@ -9,14 +9,6 @@
 // commands
 #define DEV_PATH "" // the path the device is placed
 
-// constants
-#define PAGE 0x1000
-#define FAULT_ADDR 0xdead0000
-#define FAULT_OFFSET PAGE
-#define MMAP_SIZE 4 * PAGE
-#define FAULT_SIZE MMAP_SIZE - FAULT_OFFSET
-// (END constants)
-
 // root account without password
 static char g_root[] = "root::0:0:root:/root:/bin/sh\n\n\n";
 static_assert(sizeof(g_root) == 0x20);
